6c210823effd897763d8d7195e5c5b533f55b3b4
Omar Maskati <70995663+omaskati@users.noreply.github.com>
2023-12-14 15:34:57 -0500

    Add table and populate with default library

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const myLibrary = [];
+const tableBody = document.querySelector("#book-list>tbody");
 
 function Book(title, author, numPages, isRead){
     this.title = title;
@@ -19,10 +20,36 @@ function addBookToLibrary(book){
     else return false;
 }
 
-addBookToLibrary(new Book("Green Eggs and Ham", "Dr. Seuss", 65, true));
-addBookToLibrary(new Book("Where the Wild Things Are", "Maurice Sendak", 40, true));
-addBookToLibrary(new Book("The Very Hungry Caterpillar", "Eric Carle", 26, true));
+function initDefaultLibrary(){
+    addBookToLibrary(new Book("Green Eggs and Ham", "Dr. Seuss", 65, true));
+    addBookToLibrary(new Book("Where the Wild Things Are", "Maurice Sendak", 40, true));
+    addBookToLibrary(new Book("The Very Hungry Caterpillar", "Eric Carle", 26, true));
+}
 
-myLibrary.forEach((book)=>{
-    console.log(book.title)});
+// myLibrary.forEach((book)=>{console.log(book.title)});
+
+
+function populateTable(){
+    for(let i = 0; i< myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookRow = document.createElement("tr");
+        let countCell = document.createElement("td");
+        countCell.innerText=i+1;
+        bookRow.appendChild(countCell);
+        for(let property in book){
+            if(property !== "info"){
+                let cell= document.createElement("td");
+                cell.innerText=book[property];
+                bookRow.appendChild(cell);
+            }
+        }
+        tableBody.appendChild(bookRow);
+    }
+}
+
+/*INIT PAGE*/
+initDefaultLibrary();
+populateTable();
+
+
 
